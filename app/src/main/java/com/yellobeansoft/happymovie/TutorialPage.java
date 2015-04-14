c474924cc346bda179f16bca4eff41e99b7a2d39
beboyz <bel3oyz@gmail.com>
2015-04-15 00:02:49 +0700

    add tutorial page

--- app/src/main/java/com/yellobeansoft/happymovie/TutorialPage.java
+++ app/src/main/java/com/yellobeansoft/happymovie/TutorialPage.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 
     public static final String ARG_SECTION_NUMBER = "section_number";
     Button btnExit;
-    TextView txtTutor;
+    TextView txtTutor , txtTutorEn , txtTutorTitle;
     ImageView imgTutor;
         public static TutorialPage newInstance() {
             TutorialPage fragment = new TutorialPage();
@@ -29,6 +29,8 @@ import android.widget.TextView;
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.layout_tutorial_page, container, false);
             txtTutor = (TextView) rootView.findViewById(R.id.txtTutor);
+            txtTutorEn = (TextView) rootView.findViewById(R.id.txtTutorEn);
+            txtTutorTitle = (TextView) rootView.findViewById(R.id.txtTutorTitle);
             imgTutor = (ImageView) rootView.findViewById(R.id.imgTutor);
             btnExit = (Button) rootView.findViewById(R.id.btnExit);
             btnExit.setOnClickListener(new View.OnClickListener() {
@@ -43,34 +45,50 @@ import android.widget.TextView;
             switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
                 case 1:
                     txtTutor.setText(getString(R.string.tutor_th1));
+                    txtTutorEn.setText(getString(R.string.tutor_en1));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt1));
                     imgTutor.setImageResource(R.drawable.tutor1);
                     break;
                 case 2:
                     txtTutor.setText(getString(R.string.tutor_th2));
+                    txtTutorEn.setText(getString(R.string.tutor_en2));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt2));
                     imgTutor.setImageResource(R.drawable.tutor2);
                     break;
                 case 3:
                     txtTutor.setText(getString(R.string.tutor_th3));
+                    txtTutorEn.setText(getString(R.string.tutor_en3));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt3));
                     imgTutor.setImageResource(R.drawable.tutor3);
                     break;
                 case 4:
                     txtTutor.setText(getString(R.string.tutor_th4));
+                    txtTutorEn.setText(getString(R.string.tutor_en4));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt4));
                     imgTutor.setImageResource(R.drawable.tutor4);
                     break;
                 case 5:
                     txtTutor.setText(getString(R.string.tutor_th5));
+                    txtTutorEn.setText(getString(R.string.tutor_en5));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt5));
                     imgTutor.setImageResource(R.drawable.tutor5);
                     break;
                 case 6:
                     txtTutor.setText(getString(R.string.tutor_th6));
+                    txtTutorEn.setText(getString(R.string.tutor_en6));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt6));
                     imgTutor.setImageResource(R.drawable.tutor6);
                     break;
                 case 7:
                     txtTutor.setText(getString(R.string.tutor_th7));
+                    txtTutorEn.setText(getString(R.string.tutor_en7));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt7));
                     imgTutor.setImageResource(R.drawable.tutor7);
                     break;
                 case 8:
                     txtTutor.setText(getString(R.string.tutor_th8));
+                    txtTutorEn.setText(getString(R.string.tutor_en8));
+                    txtTutorTitle.setText(getString(R.string.tutor_tt8));
                     imgTutor.setImageResource(R.drawable.tutor8);
                     break;
                 default:
